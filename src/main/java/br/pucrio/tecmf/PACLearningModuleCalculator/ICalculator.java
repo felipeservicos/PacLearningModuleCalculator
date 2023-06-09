33b1c5f8cd23242e578feb1b74c91ddf0cb50501
fr22497 <fr22497@furnas.com.br>
2023-06-09 18:21:11 -0300

    Interface suggestion with one method for calculate.

---
+++ src/main/java/br/pucrio/tecmf/PACLearningModuleCalculator/ICalculator.java
@@ -0,0 +1,9 @@
+package br.pucrio.tecmf.PACLearningModuleCalculator;
+
+public interface ICalculator {
+
+    //TODO add more methods for PAC Learning
+
+    public Integer calculate(Integer accuracy, Integer reliability, Integer VC_Dim);
+
+}
