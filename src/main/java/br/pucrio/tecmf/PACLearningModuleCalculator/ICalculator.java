cc755498df1ea32e9c23f75a8dcc1d86b62f5785
Felipe Lopes <fr22497>
2023-06-10 14:05:54 -0300

    App structure planning v1

--- src/main/java/br/pucrio/tecmf/PACLearningModuleCalculator/ICalculator.java
+++
@@ -1,9 +0,0 @@
-package br.pucrio.tecmf.PACLearningModuleCalculator;
-
-public interface ICalculator {
-
-    //TODO add more methods for PAC Learning
-
-    public Integer calculate(Integer accuracy, Integer reliability, Integer VC_Dim);
-
-}
---
+++ src/main/java/br/pucrio/tecmf/PACLearningModuleCalculator/service/LinearRegressionCalculator.java
@@ -0,0 +1,35 @@
+package br.pucrio.tecmf.PACLearningModuleCalculator.service;
+
+public class LinearRegressionCalculator implements IPACCalculator{
+    /**
+     * @param accuracy 
+     * @param reliability
+     * @param VCDim
+     * @return
+     */
+    @Override
+    public Integer calculateMinimalSample(Integer accuracy, Integer reliability, Integer VCDim) {
+        return null;
+    }
+
+    /**
+     * @param features 
+     * @return
+     */
+    @Override
+    public Integer estimateVCDim(Integer features) {
+        return null;
+    }
+
+    /**
+     * @param accuracy 
+     * @param reliability
+     * @param VCDim
+     * @param range
+     * @return
+     */
+    @Override
+    public Integer[][] lowerBoundsSamplesBetweenAccuracyAndReliability(Integer[] accuracy, Integer[] reliability, Integer VCDim, Integer range) {
+        return new Integer[0][];
+    }
+}
