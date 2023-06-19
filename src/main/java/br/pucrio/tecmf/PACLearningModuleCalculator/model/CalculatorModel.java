27d0e6ce19fcf9779f0da183bb1be69c5ac57dce
Felipe Lopes <fr22497>
2023-06-19 13:14:13 -0300

    .

--- src/main/java/br/pucrio/tecmf/PACLearningModuleCalculator/model/CalculatorModel.java
+++ src/main/java/br/pucrio/tecmf/PACLearningModuleCalculator/model/CalculatorModel.java
@@ -3,7 +3,7 @@ package br.pucrio.tecmf.PACLearningModuleCalculator.model;
 public class CalculatorModel  {
 
     private Integer accuracy;
-    private Integer reliability
+    private Integer reliability;
     private Integer VCDim; // ??????? Builder
     //TODO create a builder pattern
 
